fe2014c646322f0e946f6dcad7714db0fc534898
Wuchenyu6 <734454383@qq.com>
2019-03-13 18:54:30 +0800

    v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,35 @@ module.exports = function (grunt) {
         htmlhintrc:'.htmlhintrc'    
       },
       src:'*.html'
-    }  
-  });
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-htmlhint');
+    },
+  htmlmin:{
+    options: {
+      collapseWhitespace: true,
+      preserveLineBreaks: false
+    },
+    files: {
+      src: './index.html',
+      dest:'dist/index.html'                       
+    }
+  },
+  cssmin: {
+    'dist/rectangle.css': 'rectangle.css'
+  },
+  uglify: {
+    release:{
+      files: {
+        'dist/rectangle.js': 'rectangle.js',
+        'dist/calc.js': 'calc.js'
+      }
+    }
+  }
+});
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-htmlhint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');   
   grunt.registerTask('lint', ['htmlhint','csslint','eslint']);
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
